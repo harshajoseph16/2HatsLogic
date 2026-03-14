221dc57d1a31d373cb6e2ce9789997015f1fc85b
harsha <harshajoseph16@gmail.com>
2026-03-14 10:24:29 +0530

    search

---
+++ js/search.js
@@ -0,0 +1,7 @@
+<script>const searchBox = document.getElementById("searchBox");
+const searchInput = document.querySelector(".search-input");
+
+searchBox.addEventListener("click", function () {
+    searchBox.classList.add("active");
+    searchInput.focus();
+});</script>
